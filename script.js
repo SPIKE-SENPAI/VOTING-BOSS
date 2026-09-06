102205c2958389cdc18a6e1b22368f03ffe8159d
SPIKE-SENPAI <muhammadfatihizzulhikam@gmail.com>
2026-09-06 19:42:19 +0700

    Rename script.js to home/script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.isIntersecting){entry.target.classList.add("show");observer.unobserve(entry.target)}})},{threshold:.12});document.querySelectorAll(".reveal").forEach(el=>observer.observe(el));
